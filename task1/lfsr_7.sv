6c1ed8d203b436c697ebc1a36c95431fb2cd08dc
GiannisChristodoulou1 <chbjohn1@gmail.com>
2024-11-06 03:35:11 +0000

    did tasks 1 2 and 3

--- task1/lfsr_7.sv
+++ task1/lfsr_7.sv
@@ -4,5 +4,15 @@ module lfsr_7 (
     input   logic       en,
     output  logic [6:0] data_out
 );
+    logic   [4:1]   sreg;
+
+    always_ff @(posedge clk, posedge rst)
+
+        if (rst)
+            sreg <= 4'b1;
+        else if (en)
+            sreg <= {sreg[3:1], sreg[3]^sreg[4]};
+
+    assign data_out = sreg;
 
 endmodule
---
+++ task2/f1_fsm_tb.cpp
@@ -0,0 +1,63 @@
+#include "Vf1_fsm.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+
+int main(int argc, char argv, char env)
+{
+
+    int tick;
+    int simcyc;
+
+    Verilated::commandArgs(argc, argv);
+
+    // init top veriloginstance
+    Vf1_fsm top = new Vf1_fsm;
+
+    // init trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdCtfp = new VerilatedVcdC;
+    top->trace(tfp, 99);
+    tfp->open("f1_fsm.vcd");
+
+    // init Vbuddy
+    if (vbdOpen() != 1)
+        return (-1);
+    vbdHeader("LAB 3: F1 FSM");
+
+    // set one shot behaviour for flag
+    vbdSetMode(1);
+
+    // init simulation inputs
+    top->clk = 1;
+    top->rst = 1;
+    top->en = 0;
+
+    // run simulation
+    for (simcyc = 0; simcyc < 10000; simcyc++)
+    {
+        for (tick = 0; tick < 2; tick++)
+        {
+            tfp->dump(2 * simcyc + tick);
+            top->clk = !top->clk;
+            top->eval();
+        }
+
+        // display on Vbuddy
+        vbdBar(top->data_out);
+        vbdHex(2, (top->data_out >> 4) & 0xF);
+        vbdHex(1, top->data_out & 0xF);
+        vbdCycle(simcyc);
+
+        // get input for enable
+        top->en = vbdFlag();
+        top->rst = simcyc < 2;
+
+        if (Verilated::gotFinish() || vbdGetkey() == 'q')
+            exit(0);
+    }
+
+    vbdClose();
+    tfp->close();
+    exit(0);
+}
